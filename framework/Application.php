9fe35267089e370a6693bc744cf832b81ef75ca0
zyimm <799783009@qq.com>
2017-03-22 17:45:15 +0800

    Application init

--- framework/Application.php
+++ framework/Application.php
@@ -6,9 +6,25 @@ class Application
 
     public static $app;
 
+    
     public static $starRunTime;
 
     public static $endRunTime;
+    
+    // 用于保存单例Singleton对象，以对象类型为键
+    private $_singletons = [];
+    
+    // 用于保存依赖的定义，以对象类型为键
+    private $_definitions = [];
+    
+    // 用于保存构造函数的参数，以对象类型为键
+    private $_params = [];
+    
+    // 用于缓存ReflectionClass对象，以类名或接口名为键
+    private $_reflections = [];
+    
+    // 用于缓存依赖信息，以类名或接口名为键
+    private $_dependencies = [];
 
     public function run()
     {
@@ -23,10 +39,8 @@ class Application
     }
 
     public function has()
-    {
-        
-    }
-    
+    {}
+
     public function set()
     {}
 
@@ -35,7 +49,7 @@ class Application
 
     public function bulid()
     {}
-    
+
     public function registerError()
     {
         Error::register();
